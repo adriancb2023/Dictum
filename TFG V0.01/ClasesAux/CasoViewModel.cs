a5173b89a176afd48cb828a3ba150b56ec9f5cff
Adrian Cruz Barranco <145365245+adriancb2023@users.noreply.github.com>
2025-05-20 18:41:15 +0200

    Revert "Paco"

---
+++ TFG V0.01/ClasesAux/CasoViewModel.cs
@@ -0,0 +1,9 @@
+﻿public class CasoViewModel
+{
+    public int id { get; set; }
+    public string referencia { get; set; }
+    public string nombre_cliente { get; set; }
+    public string tipo_nombre { get; set; }
+    public string estado { get; set; }
+    public string estado_color { get; set; }
+}

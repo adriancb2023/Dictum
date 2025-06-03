2072df8c6855f510b8bab9dfc3c5b0c42c2d0b31
Adrian Cruz Barranco <145365245+adriancb2023@users.noreply.github.com>
2025-06-03 20:56:20 +0200

    ventana gpt

--- TFG V0.01/Ventanas/GPTChat.xaml.cs
+++ TFG V0.01/Ventanas/GPTChat.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using TFG_V0._01.Helpers;
 
 namespace TFG_V0._01.Ventanas
 {
@@ -19,9 +21,152 @@ namespace TFG_V0._01.Ventanas
     /// </summary>
     public partial class GPTChat : Window
     {
+        private static Storyboard meshAnimStoryboard;
+        private static RadialGradientBrush mesh1Brush;
+        private static RadialGradientBrush mesh2Brush;
+
+        static GPTChat()
+        {
+            meshAnimStoryboard = new Storyboard();
+            mesh1Brush = new RadialGradientBrush();
+            mesh2Brush = new RadialGradientBrush();
+        }
+
         public GPTChat()
         {
             InitializeComponent();
+            this.DataContext = this;
+            
+            InitializeAnimations();
+            CrearFondoAnimado();
+            AplicarModoSistema();
+            BeginFadeInAnimation();
+
+            navbar.ActualizarIdioma(MainWindow.idioma);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Aquí irá la lógica de carga inicial
+        }
+
+        private void InitializeAnimations()
+        {
+            // Inicializar animaciones si es necesario
+        }
+
+        private void CrearFondoAnimado()
+        {
+            // Crear los brushes
+            mesh1Brush = new RadialGradientBrush();
+            mesh1Brush.Center = new Point(0.3, 0.3);
+            mesh1Brush.RadiusX = 0.5;
+            mesh1Brush.RadiusY = 0.5;
+            mesh1Brush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#de9cb8"), 0));
+            mesh1Brush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#9dcde1"), 1));
+            mesh1Brush.Freeze();
+            mesh1Brush = mesh1Brush.Clone();
+
+            mesh2Brush = new RadialGradientBrush();
+            mesh2Brush.Center = new Point(0.7, 0.7);
+            mesh2Brush.RadiusX = 0.6;
+            mesh2Brush.RadiusY = 0.6;
+            mesh2Brush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#dc8eb8"), 0));
+            mesh2Brush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#98d3ec"), 1));
+            mesh2Brush.Freeze();
+            mesh2Brush = mesh2Brush.Clone();
+
+            // Crear el DrawingBrush
+            var drawingGroup = new DrawingGroup();
+            drawingGroup.Children.Add(new GeometryDrawing(mesh1Brush, null, new RectangleGeometry(new Rect(0, 0, 1, 1))));
+            drawingGroup.Children.Add(new GeometryDrawing(mesh2Brush, null, new RectangleGeometry(new Rect(0, 0, 1, 1))));
+            meshGradientBrush = new DrawingBrush(drawingGroup) { Stretch = Stretch.Fill };
+            ((Grid)this.Content).Background = meshGradientBrush;
+        }
+
+        private void AplicarModoSistema()
+        {
+            this.Tag = MainWindow.isDarkTheme;
+            var button = FindName("ThemeButton") as Button;
+            var icon = button?.Template.FindName("ThemeIcon", button) as Image;
+
+            // Cambiar fondo mesh gradient
+            if (MainWindow.isDarkTheme)
+            {
+                if (icon != null)
+                    icon.Source = new BitmapImage(new Uri("/TFG V0.01;component/Recursos/Iconos/sol.png", UriKind.Relative));
+                // Colores mesh oscuro
+                mesh1Brush.GradientStops[0].Color = (Color)ColorConverter.ConvertFromString("#8C7BFF");
+                mesh1Brush.GradientStops[1].Color = (Color)ColorConverter.ConvertFromString("#08a693");
+                mesh2Brush.GradientStops[0].Color = (Color)ColorConverter.ConvertFromString("#3a4d5f");
+                mesh2Brush.GradientStops[1].Color = (Color)ColorConverter.ConvertFromString("#272c3f");
+            }
+            else
+            {
+                if (icon != null)
+                    icon.Source = new BitmapImage(new Uri("/TFG V0.01;component/Recursos/Iconos/luna.png", UriKind.Relative));
+                // Colores mesh claro
+                mesh1Brush.GradientStops[0].Color = (Color)ColorConverter.ConvertFromString("#de9cb8");
+                mesh1Brush.GradientStops[1].Color = (Color)ColorConverter.ConvertFromString("#9dcde1");
+                mesh2Brush.GradientStops[0].Color = (Color)ColorConverter.ConvertFromString("#dc8eb8");
+                mesh2Brush.GradientStops[1].Color = (Color)ColorConverter.ConvertFromString("#98d3ec");
+            }
+
+            navbar.ActualizarTema(MainWindow.isDarkTheme);
+            IniciarAnimacionMesh();
+        }
+
+        private void ThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.isDarkTheme = !MainWindow.isDarkTheme;
+            AplicarModoSistema();
+            var fadeAnimation = CrearFadeAnimation(0.7, 0.9, 0.3, true);
+            this.BeginAnimation(OpacityProperty, fadeAnimation);
+        }
+
+        private void BeginFadeInAnimation()
+        {
+            this.Opacity = 0;
+            var fadeInAnimation = CrearFadeAnimation(0, 1, 0.5);
+            this.BeginAnimation(OpacityProperty, fadeInAnimation);
+        }
+
+        private DoubleAnimation CrearFadeAnimation(double from, double to, double durationSeconds, bool autoReverse = false) =>
+            new()
+            {
+                From = from,
+                To = to,
+                Duration = TimeSpan.FromSeconds(durationSeconds),
+                AutoReverse = autoReverse
+            };
+
+        private void IniciarAnimacionMesh()
+        {
+            // Detener si ya existe
+            meshAnimStoryboard?.Stop();
+            meshAnimStoryboard = new Storyboard();
+            
+            // Animar Center de mesh1
+            var anim1 = new PointAnimationUsingKeyFrames();
+            anim1.KeyFrames.Add(new EasingPointKeyFrame(new Point(0.3, 0.3), KeyTime.FromTimeSpan(TimeSpan.Zero)));
+            anim1.KeyFrames.Add(new EasingPointKeyFrame(new Point(0.7, 0.5), KeyTime.FromTimeSpan(TimeSpan.FromSeconds(4))) { EasingFunction = new SineEase { EasingMode = EasingMode.EaseInOut } });
+            anim1.KeyFrames.Add(new EasingPointKeyFrame(new Point(0.3, 0.3), KeyTime.FromTimeSpan(TimeSpan.FromSeconds(8))) { EasingFunction = new SineEase { EasingMode = EasingMode.EaseInOut } });
+            anim1.RepeatBehavior = RepeatBehavior.Forever;
+            Storyboard.SetTarget(anim1, mesh1Brush);
+            Storyboard.SetTargetProperty(anim1, new PropertyPath(RadialGradientBrush.CenterProperty));
+            meshAnimStoryboard.Children.Add(anim1);
+
+            // Animar Center de mesh2
+            var anim2 = new PointAnimationUsingKeyFrames();
+            anim2.KeyFrames.Add(new EasingPointKeyFrame(new Point(0.7, 0.7), KeyTime.FromTimeSpan(TimeSpan.Zero)));
+            anim2.KeyFrames.Add(new EasingPointKeyFrame(new Point(0.4, 0.4), KeyTime.FromTimeSpan(TimeSpan.FromSeconds(4))) { EasingFunction = new SineEase { EasingMode = EasingMode.EaseInOut } });
+            anim2.KeyFrames.Add(new EasingPointKeyFrame(new Point(0.7, 0.7), KeyTime.FromTimeSpan(TimeSpan.FromSeconds(8))) { EasingFunction = new SineEase { EasingMode = EasingMode.EaseInOut } });
+            anim2.RepeatBehavior = RepeatBehavior.Forever;
+            Storyboard.SetTarget(anim2, mesh2Brush);
+            Storyboard.SetTargetProperty(anim2, new PropertyPath(RadialGradientBrush.CenterProperty));
+            meshAnimStoryboard.Children.Add(anim2);
+            
+            meshAnimStoryboard.Begin();
         }
     }
 }
